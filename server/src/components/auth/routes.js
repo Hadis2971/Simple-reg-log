5e3c6561f40fb5ab6454b4c922d663473c8a2074
Albert Hofmann <albhof@email.com>
2019-04-28 18:15:19 +0200

    add validation and sanitize for login input

--- server/src/components/auth/routes.js
+++ server/src/components/auth/routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 import { validateRegisterInput, sanitizeRegisterInput } from "./validateSanitize";
+import { validateLoginInput, sanitizeLoginInput } from "./validateSanitize";
 import { registerUser } from "./registerUser";
 
 router.post("/register", validateRegisterInput, sanitizeRegisterInput, registerUser);
 
-router.post("/login");
+router.post("/login", validateLoginInput, sanitizeLoginInput);
 
 export default router;
--- server/src/components/auth/validateSanitize.js
+++ server/src/components/auth/validateSanitize.js
@@ -1,6 +1,9 @@
 import registerSanitize from "../../input_check/sanitize/register";
 import registerValidation from "../../input_check/validate/register";
 
+import loginValidation from "../../input_check/validate/login";
+import loginSanitize from "../../input_check/sanitize/login";
+
 export const validateRegisterInput = (req, res, next) => {
     const { errors, isValid } = registerValidation(req.body);
     if (!isValid) {
@@ -10,5 +13,17 @@ export const validateRegisterInput = (req, res, next) => {
 
 export const sanitizeRegisterInput = (req, res, next) => {
     registerSanitize(req.body);
+    next();
+};
+
+export const validateLoginInput = (req, res, next) => {
+    const { errors, isValid } = loginValidation(req.body);
+    if (!isValid) {
+        res.status(400).json({Error: errors});
+    } else next();
+};
+
+export const sanitizeLoginInput = (req, res, next) => {
+    loginSanitize(req.body);
     next();
 };
